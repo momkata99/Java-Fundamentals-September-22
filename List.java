a94bdbe60eed882b6fa0d7a18d01bbd5fdd39c25
momkata99 <114524249+momkata99@users.noreply.github.com>
2022-09-28 01:46:08 +0300

    First exersice

---
+++ List.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class List {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       int a = Integer.parseInt(scanner.nextLine());
+        System.out.println(a);
+
+    }
+
+    }
+
+
+
+
